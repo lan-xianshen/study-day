351837239ae73b0663956618a1095af40ec6968c
lan-xianshen <15584349303@163.com>
2021-04-28 17:49:18 +0800

    xiugai

---
+++ gof/src/main/java/singleton/SingletonFirst.java
@@ -0,0 +1,11 @@
+package singleton;
+
+/**
+ * @author jianglin.lan
+ * @title: SingletonFirst
+ * @projectName study-day
+ * @description: TODO
+ * @date 2021/4/2817:48
+ */
+public class SingletonFirst {
+}
