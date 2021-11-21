94500d6b48487a0a8df93b8d7c8056a50d3c433a
lan-xianshen <15584349303@163.com>
2021-11-21 16:24:56 +0800

    'hotfix002'

---
+++ juc/src/main/java/com/lan/future/TestGitBranch.java
@@ -0,0 +1,11 @@
+package com.lan.future;
+
+/**
+ * @author lan
+ * @create 2021-11-21 16:21
+ */
+public class TestGitBranch {
+    public static void main(String[] args) {
+        System.out.println("这是git 的第二个分支 hotfix");
+    }
+}
