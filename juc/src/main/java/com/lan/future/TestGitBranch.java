9086e7465f9a1138c4fb9bf52400cd0f9c2424bf
lan-xianshen <15584349303@163.com>
2021-11-21 17:16:44 +0800

    git 02分支上提交 test第二次

--- juc/src/main/java/com/lan/future/TestGitBranch.java
+++ juc/src/main/java/com/lan/future/TestGitBranch.java
@@ -7,5 +7,8 @@ package com.lan.future;
 public class TestGitBranch {
     public static void main(String[] args) {
         System.out.println("这是git 的第二个分支 hotfix");
+        System.out.println("这是git 的第二个分支 hotfix");
+        System.out.println("这是git 的第二个分支 hotfix");
+        System.out.println("这是git 的第二个分支 hotfix");
     }
 }
