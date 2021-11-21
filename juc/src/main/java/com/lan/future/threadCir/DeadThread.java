20f9db7bac2d4a34ac6b34923fa757116c112d89
lan-xianshen <15584349303@163.com>
2021-11-21 16:13:58 +0800

    '第一个分支提交'

--- juc/src/main/java/com/lan/future/threadCir/DeadThread.java
+++ juc/src/main/java/com/lan/future/threadCir/DeadThread.java
@@ -20,13 +20,11 @@ public class DeadThread implements Runnable{
     public void run() {
         synchronized (lockA){
             System.out.println(Thread.currentThread ().getName ()+"线程获得"+lockA);
-
             try {
                 TimeUnit.SECONDS.sleep (2);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
             synchronized (lockB){
                 System.out.println(Thread.currentThread ().getName ()+"线程获得"+lockB);
 
---
+++ springMVC/target/springMVC/WEB-INF/page/success.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jianglin.lan
+  Date: 2021/7/19
+  Time: 15:02
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>success ....</h1>
+</body>
+</html>
